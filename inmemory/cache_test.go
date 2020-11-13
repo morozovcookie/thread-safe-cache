690fc109cd0c77f12c9a34fcd4d3b738592724a8
Konstantin Morozov <morozov.cookie@gmail.com>
2020-11-13 17:02:30 +0300

    test(cache): concurrent tests

--- inmemory/cache_test.go
+++ inmemory/cache_test.go
@@ -1,7 +1,11 @@
 package inmemory
 
 import (
+	"context"
+	"math/rand"
+	"sync"
 	"testing"
+	"time"
 
 	tsc "github.com/morozovcookie/threadsafecache"
 	"github.com/stretchr/testify/assert"
@@ -102,6 +106,182 @@ func TestCache_GetOrSet(t *testing.T) {
 	}
 }
 
-//func TestThousandGoroutines(t *testing.T) {
-//
-//}
+func TestValueFnWillCallOnlyOne(t *testing.T) {
+	var (
+		cache = NewCache()
+
+		key = "test-key"
+		i   = 0
+		fn  = func() tsc.Value {
+			i++
+			return "test-value"
+		}
+
+		expectedVal = "test-value"
+	)
+
+	assert.Equal(t, expectedVal, cache.GetOrSet(key, fn))
+	assert.Equal(t, 1, i)
+
+	assert.Equal(t, expectedVal, cache.GetOrSet(key, fn))
+	assert.Equal(t, 1, i)
+}
+
+func goroutineCacheGetOrSet(
+	ctx context.Context,
+	t *testing.T,
+	idx int,
+	wg *sync.WaitGroup,
+	cache *Cache,
+	key tsc.Key,
+	fn func() tsc.Value,
+	expectedVal tsc.Value) {
+	//t.Logf("starting goroutine #%d", idx)
+
+	for {
+		select {
+		case <-time.After(time.Millisecond * 10):
+			actualVal := cache.GetOrSet(key, fn)
+			//t.Logf("[goroutine #%d] GetOrSet(%s, fn) = %s", idx, key, actualVal)
+			assert.Equal(t, expectedVal, actualVal)
+		case <-ctx.Done():
+			//t.Logf("stopping goroutine #%d", idx)
+			wg.Done()
+			return
+		}
+	}
+}
+
+func goroutineCacheGetOrGetOrSet(
+	ctx context.Context,
+	t *testing.T,
+	idx int,
+	wg *sync.WaitGroup,
+	cache *Cache,
+	key tsc.Key,
+	fn func() tsc.Value,
+	expectedVal tsc.Value) {
+	//t.Logf("starting goroutine #%d", idx)
+
+	arr := []func(t *testing.T, cache *Cache, key tsc.Key, fn func() tsc.Value) tsc.Value{
+		func(t *testing.T, cache *Cache, key tsc.Key, _ func() tsc.Value) tsc.Value {
+			val, _ := cache.Get(key)
+
+			//t.Logf("[goroutine #%d] Get(%s) = %s", idx, key, val)
+
+			return val
+		},
+		func(t *testing.T, cache *Cache, key tsc.Key, fn func() tsc.Value) tsc.Value {
+			val := cache.GetOrSet(key, fn)
+
+			//t.Logf("[goroutine #%d] GetOrSet(%s, fn) = %s", idx, key, val)
+
+			return val
+		},
+	}
+
+	for {
+		select {
+		case <-time.After(time.Millisecond * 10):
+			assert.Equal(t, expectedVal, arr[rand.Intn(2)](t, cache, key, fn))
+		case <-ctx.Done():
+			//t.Logf("stopping goroutine #%d", idx)
+			wg.Done()
+			return
+		}
+	}
+}
+
+func callGetOrSetConcurrent(t *testing.T, n int, testTime time.Duration) {
+	var (
+		cache = NewCache()
+
+		key = "test-key"
+		fn  = func() tsc.Value {
+			return "test-value"
+		}
+
+		expectedVal = "test-value"
+
+		wg = sync.WaitGroup{}
+	)
+
+	wg.Add(n)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	for i := 0; i < n; i++ {
+		go goroutineCacheGetOrSet(ctx, t, i+1, &wg, cache, key, fn, expectedVal)
+	}
+
+	<-time.After(testTime)
+	cancel()
+
+	wg.Wait()
+}
+
+func callGetOrGetOrSetConcurrent(t *testing.T, n int, testTime time.Duration) {
+	var (
+		cache = NewCache()
+
+		key = "test-key"
+		fn  = func() tsc.Value {
+			return "test-value"
+		}
+
+		expectedVal = "test-value"
+
+		wg = sync.WaitGroup{}
+	)
+
+	wg.Add(n)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	for i := 0; i < n; i++ {
+		go goroutineCacheGetOrGetOrSet(ctx, t, i+1, &wg, cache, key, fn, expectedVal)
+	}
+
+	<-time.After(testTime)
+	cancel()
+
+	wg.Wait()
+}
+
+func TestGetOrSetConcurrentCalls(t *testing.T) {
+	callGetOrSetConcurrent(t, 2, time.Second)
+}
+
+func TestGetOrSet_1_000_Goroutines(t *testing.T) {
+	callGetOrSetConcurrent(t, 1_000, time.Second)
+}
+
+func TestGetOrSet_10_000_Goroutines(t *testing.T) {
+	callGetOrSetConcurrent(t, 10_000, time.Minute)
+}
+
+func TestGetOrSet_100_000_Goroutines(t *testing.T) {
+	callGetOrSetConcurrent(t, 100_000, time.Minute)
+}
+
+func TestGetOrSet_1_000_000_Goroutines(t *testing.T) {
+	callGetOrSetConcurrent(t, 1_000_000, time.Minute)
+}
+
+func TestGetOrGetOrSetConcurrentCalls(t *testing.T) {
+	callGetOrGetOrSetConcurrent(t, 2, time.Second)
+}
+
+func TestGetOrGetOrSet_1_000_Goroutines(t *testing.T) {
+	callGetOrGetOrSetConcurrent(t, 1_000, time.Second)
+}
+
+func TestGetOrGetOrSet_10_000_Goroutines(t *testing.T) {
+	callGetOrGetOrSetConcurrent(t, 10_000, time.Minute)
+}
+
+func TestGetOrGetOrSet_100_000_Goroutines(t *testing.T) {
+	callGetOrGetOrSetConcurrent(t, 100_000, time.Minute)
+}
+
+func TestGetOrGetOrSet_1_000_000_Goroutines(t *testing.T) {
+	callGetOrGetOrSetConcurrent(t, 1_000_000, time.Minute)
+}
